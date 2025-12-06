aad2375be7a206384d7dde6b589052d0ef726275
ChantelMokwena <163107676+ChantelMokwena@users.noreply.github.com>
2025-12-06 06:38:38 +0200

    Included messaging urls into core urls

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, Sp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include('messaging.urls')),
     
     # JWT Authentication
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
